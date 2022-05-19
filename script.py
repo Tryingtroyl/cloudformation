9f0ca7fc3a832ec16212d1cf10f39906256428d5
Tryingtroyl <aztec1311@gmail.com>
2022-05-19 11:35:22 +0530

    Second

--- script.py
+++ script.py
@@ -19,13 +19,37 @@ cfn_session=boto3.session.Session(aws_access_key_id=str(sys.argv[1]),aws_secret_
 
 cfn=cfn_session.client('cloudformation', config=my_config)
 
-print(cfn.create_stack(StackName=str(sys.argv[2]),
+print(cfn.create_stack(StackName=str(sys.argv[8]),
  TemplateURL='https://for-practice-inkwell.s3.ap-south-1.amazonaws.com/task.yml',
  Parameters=[
         {
             'ParameterKey': 'InstanceTypeChoice',
-            'ParameterValue': (str(sys.argv[1])),
+            'ParameterValue': (str(sys.argv[3])),
             'UsePreviousValue': True,
             'ResolvedValue': 'string'
         },
+        {
+            'ParameterKey': 'VPCCidr',
+            'ParameterValue': (str(sys.argv[4])),
+            'UsePreviousValue': True,
+            'ResolvedValue': 'string'
+        },
+        {
+            'ParameterKey': 'PublicSubCidr',
+            'ParameterValue': (str(sys.argv[5])),
+            'UsePreviousValue': True,
+            'ResolvedValue': 'string'
+        },
+        {
+            'ParameterKey': 'PrivateSubCidr',
+            'ParameterValue': (str(sys.argv[6])),
+            'UsePreviousValue': True,
+            'ResolvedValue': 'string'
+        },
+        {
+            'ParameterKey': 'BucketName',
+            'ParameterValue': (str(sys.argv[7])),
+            'UsePreviousValue': True,
+            'ResolvedValue': 'string'
+        }
     ]))
